9738fb8fc33fa5e4f6da1e774a1d965199606dda
RPGExodus <Raver98@wp.pl>
2017-05-23 17:31:46 +0200

    cc

--- app/src/main/java/pl/billog_studio/exodus/Autorzy.java
+++ app/src/main/java/pl/billog_studio/exodus/Autorzy.java
@@ -1,7 +1,10 @@
 package pl.billog_studio.exodus;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Autorzy extends Activity {
 
@@ -9,5 +12,13 @@ public class Autorzy extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_autorzy);
+        final Button powrot = (Button)findViewById(R.id.button_powrot);
+        powrot.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Autorzy.this, Main_Menu.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
