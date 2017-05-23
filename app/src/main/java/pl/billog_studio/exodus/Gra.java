20c8595c43a138f59e8604240ed8df9e493a5348
RPGExodus <Raver98@wp.pl>
2017-05-23 20:57:42 +0200

    cc

--- app/src/main/java/pl/billog_studio/exodus/Gra.java
+++ app/src/main/java/pl/billog_studio/exodus/Gra.java
@@ -8,25 +8,26 @@ import android.widget.Button;
 
 public class Gra extends AppCompatActivity {
     int count=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gra);
-        if (count==2)
-        {
-            Intent intent6= new Intent(Gra.this, gra_atak.class);
-            startActivity(intent6);
-            finish();
-        }
+
         final Button button = (Button)findViewById(R.id.statek_button_1_a);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 button.setVisibility(View.INVISIBLE);
                 count++;
+
+                if (count==2)
+                {
+                    Intent intent6= new Intent(Gra.this, gra_atak.class);
+                    startActivity(intent6);
+                    finish();
+                }
                 Intent intent = new Intent(Gra.this, Gra_1.class);
                 startActivity(intent);
-
-
             }
         });
         final Button button2 = (Button)findViewById(R.id.statek_button_1_b);
@@ -34,9 +35,15 @@ public class Gra extends AppCompatActivity {
             public void onClick(View v) {
                 button2.setVisibility(View.INVISIBLE);
                 count++;
+
+                if (count==2)
+                {
+                    Intent intent6= new Intent(Gra.this, gra_atak.class);
+                    startActivity(intent6);
+                    finish();
+                }
                 Intent intent2 = new Intent(Gra.this, Gra_2.class);
                 startActivity(intent2);
-
 
             }
         });
@@ -45,10 +52,15 @@ public class Gra extends AppCompatActivity {
             public void onClick(View v) {
                 button3.setVisibility(View.INVISIBLE);
                 count++;
+
+                if (count==2)
+                {
+                    Intent intent6= new Intent(Gra.this, gra_atak.class);
+                    startActivity(intent6);
+                    finish();
+                }
                 Intent intent3 = new Intent(Gra.this, Gra_3.class);
                 startActivity(intent3);
-
-
             }
         });
 
