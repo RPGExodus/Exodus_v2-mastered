84256c175cc10f50bcdadfa233ffc4e93bbfbff1
RPGExodus <Raver98@wp.pl>
2017-05-23 18:07:08 +0200

    cc

---
+++ app/src/main/java/Gra/Companions.java
@@ -0,0 +1,7 @@
+package Gra;
+
+/**
+ * Created by Raver on 2017-05-23.
+ */
+public class Companions {
+}
---
+++ app/src/main/java/Gra/Player.java
@@ -0,0 +1,16 @@
+package Gra;
+
+/**
+ * Created by Raver on 2017-05-23.
+ */
+public class Player {
+    int strenght;
+    int dextirity;
+    int intelligence;
+    int food;
+    int sleep;
+    int peste;
+    Items[] equipmnent;
+    Abillities[] abillities;
+    Companions[] team;
+}
