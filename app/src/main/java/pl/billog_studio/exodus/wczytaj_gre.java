fd2c630819f8bfdd80faf0dcb1c1f2a62e5ae824
krzysiek4321 <multikys98@wp.pl>
2017-05-24 12:35:38 +0200

    Wątpliwy update z eqportem Questów

--- app/src/main/java/pl/billog_studio/exodus/wczytaj_gre.java
+++ app/src/main/java/pl/billog_studio/exodus/wczytaj_gre.java
@@ -1,6 +1,7 @@
 package pl.billog_studio.exodus;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class wczytaj_gre extends Activity {
@@ -13,5 +14,12 @@ public class wczytaj_gre extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wczytaj_gre);
+        try {
+            Intent next=new Intent(wczytaj_gre.this,Class.forName("Gra_1").getClass());
+            startActivity(next);
+            finish();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        };
     }
 }
