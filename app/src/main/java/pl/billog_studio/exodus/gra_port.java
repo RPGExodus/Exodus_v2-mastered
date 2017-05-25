116a79bdd81f282c426ddbf9fe7c086dbaa59d8c
krzysiek4321 <multikys98@wp.pl>
2017-05-25 02:30:03 +0200

    Możliwe że sklep jest skończony

--- app/src/main/java/pl/billog_studio/exodus/gra_port.java
+++ app/src/main/java/pl/billog_studio/exodus/gra_port.java
@@ -3,11 +3,21 @@ package pl.billog_studio.exodus;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
+import Gra.*;
+
 public class gra_port extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gra_port);
+        Wszystko.lokacja=this.getClass().getName();
+        try {
+            Wszystko.saveTheGame();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
